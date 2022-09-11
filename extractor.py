cf7dae0fa359b9102603e1f6d0e7d615176ca1df
srpiroliro <srpiroliro21@gmail.com>
2022-09-11 13:08:13 +0200

    + progress bar

--- extractor.py
+++ extractor.py
@@ -1,8 +1,9 @@
 import os,json
+from tqdm import tqdm
 
 user_agents={}
 
-logs_path=""
+logs_path="/home/morlok/Documents/domlogs/"
 while not logs_path:
     logs_path=input("PATH TO LOGS: ")
     if not os.path.exits(logs_path):
@@ -12,7 +13,7 @@ while not logs_path:
         logs_path=False
         print("\tERROR: path must be a fodler, not a file.")
 
-for file in os.listdir(logs_path):
+for file in tqdm(os.listdir(logs_path)):
     if not ".error" in file and file.endswith(".log"):
         with open(os.path.join(logs_path, file), errors="ignore") as f:
             file_content=f.read()
@@ -23,7 +24,6 @@ for file in os.listdir(logs_path):
                 ua=log.strip("\"").rsplit("\"", 1)[-1]
                 if ua in user_agents: user_agents[ua]+=1
                 else: user_agents[ua]=1
-        break
 
 with open("user-agents.json", "w") as f:
     json.dump(
